84ef3ce1329a34fcfd39b42e306739e66cecf78e
QuentinPotgieter <quentinpotgieter@hotmail.co.za>
2020-09-08 16:38:53 +0200

    no message

---
+++ BooksAndAuthors/FindAuthor.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace BooksAndAuthors
+{
+    partial class FindAuthor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goneWithTheWindLabel = new System.Windows.Forms.Label();
+            this.rootsLabel = new System.Windows.Forms.Label();
+            this.booksGroup = new System.Windows.Forms.GroupBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.aynRandLabel = new System.Windows.Forms.Label();
+            this.alexHaleyLabel = new System.Windows.Forms.Label();
+            this.margaretmitchelLabel = new System.Windows.Forms.Label();
+            this.atlasShruggedLabel = new System.Windows.Forms.Label();
+            this.toggleAuthorsButton = new System.Windows.Forms.Button();
+            this.booksGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // goneWithTheWindLabel
+            // 
+            this.goneWithTheWindLabel.AutoSize = true;
+            this.goneWithTheWindLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goneWithTheWindLabel.Location = new System.Drawing.Point(34, 86);
+            this.goneWithTheWindLabel.Name = "goneWithTheWindLabel";
+            this.goneWithTheWindLabel.Size = new System.Drawing.Size(104, 13);
+            this.goneWithTheWindLabel.TabIndex = 0;
+            this.goneWithTheWindLabel.Text = "Gone with the Wind ";
+            // 
+            // rootsLabel
+            // 
+            this.rootsLabel.AutoSize = true;
+            this.rootsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rootsLabel.Location = new System.Drawing.Point(34, 110);
+            this.rootsLabel.Name = "rootsLabel";
+            this.rootsLabel.Size = new System.Drawing.Size(35, 13);
+            this.rootsLabel.TabIndex = 1;
+            this.rootsLabel.Text = "Roots";
+            // 
+            // booksGroup
+            // 
+            this.booksGroup.Controls.Add(this.titleLabel);
+            this.booksGroup.Controls.Add(this.authorLabel);
+            this.booksGroup.Controls.Add(this.aynRandLabel);
+            this.booksGroup.Controls.Add(this.alexHaleyLabel);
+            this.booksGroup.Controls.Add(this.margaretmitchelLabel);
+            this.booksGroup.Controls.Add(this.atlasShruggedLabel);
+            this.booksGroup.Controls.Add(this.rootsLabel);
+            this.booksGroup.Controls.Add(this.goneWithTheWindLabel);
+            this.booksGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.booksGroup.Location = new System.Drawing.Point(13, 13);
+            this.booksGroup.Name = "booksGroup";
+            this.booksGroup.Size = new System.Drawing.Size(272, 137);
+            this.booksGroup.TabIndex = 4;
+            this.booksGroup.TabStop = false;
+            this.booksGroup.Text = "Books";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.titleLabel.Location = new System.Drawing.Point(34, 35);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(35, 15);
+            this.titleLabel.TabIndex = 7;
+            this.titleLabel.Text = "Title";
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.AutoSize = true;
+            this.authorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorLabel.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.authorLabel.Location = new System.Drawing.Point(157, 35);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(48, 15);
+            this.authorLabel.TabIndex = 6;
+            this.authorLabel.Text = "Author";
+            this.authorLabel.Visible = false;
+            // 
+            // aynRandLabel
+            // 
+            this.aynRandLabel.AutoSize = true;
+            this.aynRandLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aynRandLabel.Location = new System.Drawing.Point(157, 63);
+            this.aynRandLabel.Name = "aynRandLabel";
+            this.aynRandLabel.Size = new System.Drawing.Size(54, 13);
+            this.aynRandLabel.TabIndex = 5;
+            this.aynRandLabel.Text = "Ayn Rand";
+            this.aynRandLabel.Visible = false;
+            // 
+            // alexHaleyLabel
+            // 
+            this.alexHaleyLabel.AutoSize = true;
+            this.alexHaleyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alexHaleyLabel.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.alexHaleyLabel.Location = new System.Drawing.Point(157, 110);
+            this.alexHaleyLabel.Name = "alexHaleyLabel";
+            this.alexHaleyLabel.Size = new System.Drawing.Size(57, 13);
+            this.alexHaleyLabel.TabIndex = 4;
+            this.alexHaleyLabel.Text = "Alex Haley";
+            this.alexHaleyLabel.Visible = false;
+            // 
+            // margaretmitchelLabel
+            // 
+            this.margaretmitchelLabel.AutoSize = true;
+            this.margaretmitchelLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.margaretmitchelLabel.Location = new System.Drawing.Point(157, 86);
+            this.margaretmitchelLabel.Name = "margaretmitchelLabel";
+            this.margaretmitchelLabel.Size = new System.Drawing.Size(86, 13);
+            this.margaretmitchelLabel.TabIndex = 3;
+            this.margaretmitchelLabel.Text = "Margaret Mitchel";
+            this.margaretmitchelLabel.Visible = false;
+            // 
+            // atlasShruggedLabel
+            // 
+            this.atlasShruggedLabel.AutoSize = true;
+            this.atlasShruggedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atlasShruggedLabel.Location = new System.Drawing.Point(34, 63);
+            this.atlasShruggedLabel.Name = "atlasShruggedLabel";
+            this.atlasShruggedLabel.Size = new System.Drawing.Size(79, 13);
+            this.atlasShruggedLabel.TabIndex = 2;
+            this.atlasShruggedLabel.Text = "Atlas Shrugged";
+            // 
+            // toggleAuthorsButton
+            // 
+            this.toggleAuthorsButton.Location = new System.Drawing.Point(184, 156);
+            this.toggleAuthorsButton.Name = "toggleAuthorsButton";
+            this.toggleAuthorsButton.Size = new System.Drawing.Size(101, 23);
+            this.toggleAuthorsButton.TabIndex = 5;
+            this.toggleAuthorsButton.Text = "Show Authors";
+            this.toggleAuthorsButton.UseVisualStyleBackColor = true;
+            this.toggleAuthorsButton.Click += new System.EventHandler(this.toggleAuthorsButton_Click);
+            // 
+            // FindAuthor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(300, 189);
+            this.Controls.Add(this.toggleAuthorsButton);
+            this.Controls.Add(this.booksGroup);
+            this.Name = "FindAuthor";
+            this.Text = "Books";
+            this.Load += new System.EventHandler(this.BookAuthors_Load);
+            this.booksGroup.ResumeLayout(false);
+            this.booksGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label goneWithTheWindLabel;
+        private System.Windows.Forms.Label rootsLabel;
+        private System.Windows.Forms.GroupBox booksGroup;
+        private System.Windows.Forms.Label atlasShruggedLabel;
+        private System.Windows.Forms.Button toggleAuthorsButton;
+        private System.Windows.Forms.Label aynRandLabel;
+        private System.Windows.Forms.Label alexHaleyLabel;
+        private System.Windows.Forms.Label margaretmitchelLabel;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ BooksAndAuthors/FindAuthor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooksAndAuthors
+{
+    public partial class FindAuthor : Form
+    {
+        private bool isActive = false;
+        public FindAuthor()
+        {
+            InitializeComponent();
+        }
+
+       private void BookAuthors_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toggleAuthorsButton_Click(object sender, EventArgs e)
+        {
+            isActive = !isActive;
+            this.authorLabel.Visible = isActive;
+            this.margaretmitchelLabel.Visible = isActive;
+            this.alexHaleyLabel.Visible = isActive;
+            this.aynRandLabel.Visible = isActive;
+            this.toggleAuthorsButton.Text = isActive ? "Hide Authors" : "Show Authors";
+        }
+    }
+}
